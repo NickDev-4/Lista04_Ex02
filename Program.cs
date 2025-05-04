194b4dd320c43450821a4f258df4200b817ed32e
NickDev-4 <nickolasfreire.santos@gmail.com>
2025-05-04 20:01:07 -0300

    Lista 4 - Exercicío 2

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+double emprestimo, renda, resultado, valorParcelas;
+int parcela;
+
+Console.Write("Digite o valor do empréstimo solicitado: ");
+emprestimo = double.Parse(Console.ReadLine());
+
+Console.Write("Digite a quantidade de parcelas para quitar o empréstimo: ");
+parcela = int.Parse(Console.ReadLine());
+
+Console.Write("Digite a sua renda mensal: ");
+renda = double.Parse(Console.ReadLine());
+
+valorParcelas = emprestimo / parcela;
+
+resultado = (valorParcelas / renda) * 100;
+
+if (resultado <= 30){
+    Console.WriteLine("Seu empréstimo foi APROVADO!");
+}
+
+else {
+    Console.WriteLine("Seu empréstimo foi NEGADO!");
+}
